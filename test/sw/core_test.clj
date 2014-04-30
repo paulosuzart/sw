4d6914b6bba8042233909df778bc17ffb4a18023
Paulo Suzart <paulo.suzart@guiato.com.br>
2014-04-30 08:15:13 -0300

    - Added a more date inserted test

--- test/sw/core_test.clj
+++ test/sw/core_test.clj
@@ -6,12 +6,13 @@
             [clojurewerkz.meltdown.selectors :refer [$ match-all]]
             [sw.core :refer :all]))
 
+
 (deftest a-test
   (testing "Simple 5 sec window"
-    (let [tickets (mr/create)
-        last-5-secs (sliding-window tickets (match-all) 5000)
-        last-sum (ref 0)]
-      (on-tick last-5-secs 
+    (let [last-sum (ref 0)
+          tickets (mr/create)
+          last-5-secs (sliding-window tickets (match-all) 5000)]
+      (on-tick last-5-secs
         (fn [b]
           (dosync
           (ref-set last-sum (apply + b)))))
@@ -26,3 +27,15 @@
       (mr/notify tickets "tenis" 20)
       (Thread/sleep 2000)
       (is (= 20 @last-sum)))))
+
+(deftest tp-test
+  (testing "Simple 5 sec window with plenty events. Should have 0 after 5 seconds"
+    (let [last-sum (ref 0)
+          tickets (mr/create)
+          last-5-secs (sliding-window tickets (match-all) 5000)]
+      (on-tick last-5-secs (fn [b] (dosync (ref-set last-sum (apply + b)))))
+      (time
+        (doall (repeatedly 10000
+          (fn  [] (mr/notify tickets "a product" (rand-int 50)) (Thread/sleep (+ 1 (rand-int 3)))))))
+      (Thread/sleep 6000)
+      (is (= 0 @last-sum)))))
